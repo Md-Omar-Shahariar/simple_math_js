9d32b2518add6a15552c7fbbd6c7b0bc4ecd141a
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-01-31 23:22:04 +0600

    random power squrt

--- math.js
+++ math.js
@@ -6,6 +6,10 @@
 const myNumber  = 6.4398
 // const output = Math.ceil(myNumber)
 // const output = Math.floor(myNumber)
-const output = Math.round(myNumber)
+// const output = Math.round(myNumber)
+// const output = Math.random()
+// const output = Math.pow(2,3)
+const output = Math.sqrt(myNumber)
+
 
 console.log(output);
