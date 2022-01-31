8852ec3e9f12af5b1f744dae5cad726ae421e63c
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-01-31 23:57:40 +0600

    destructuring approch

--- swap.js
+++ swap.js
@@ -1,7 +1,15 @@
 var first =5 
 var second = 7
-console.log(first , second)
-var temp = first
-first = second
-second = temp
-console.log(first,second)
+// console.log(first , second)
+//first approch
+// var temp = first
+// first = second
+// second = temp
+// console.log(first,second)
+
+//destructuring approch
+
+console.log(first , second);
+
+[first , second] = [second , first]
+console.log(first , second);
