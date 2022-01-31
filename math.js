3aa8e9e0eb3f8a090b46da7609d3e641e065f2b0
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-01-31 23:40:04 +0600

    pi max min

--- math.js
+++ math.js
@@ -3,13 +3,23 @@
 
 // console.log(output);
 
-const myNumber  = 6.4398
+const myNumber  = 6.43
+const arr = [12,23,45,45,44,45,6,64,42,77]
 // const output = Math.ceil(myNumber)
 // const output = Math.floor(myNumber)
 // const output = Math.round(myNumber)
 // const output = Math.random()
 // const output = Math.pow(2,3)
-const output = Math.sqrt(myNumber)
+// const output = Math.sqrt(myNumber)
+// const output = Math.sqrt(myNumber)
+
+// const output = Math.PI
+
+// const output = Math.fround(myNumber)
+
+// const output = Math.hypot(10,40)
+console.log(Math.min(12,23,45,45,44,45,6,64,42,77));
+console.log(Math.max(12,23,45,45,44,45,6,64,42,77));
 
 
 console.log(output);
