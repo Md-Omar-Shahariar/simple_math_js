3a0e7b8241cf58fa8bc92302c9b9f25cb18736d1
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-01-31 23:14:30 +0600

    ceilng floor round

--- math.js
+++ math.js
@@ -1,4 +1,11 @@
-const myNumber = -5
-const output = Math.abs(myNumber)
+// const myNumber = -5
+// const output = Math.abs(myNumber)
+
+// console.log(output);
+
+const myNumber  = 6.4398
+// const output = Math.ceil(myNumber)
+// const output = Math.floor(myNumber)
+const output = Math.round(myNumber)
 
 console.log(output);
