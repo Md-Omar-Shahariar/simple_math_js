1995dbc1ff171a0a0bc4cb1e5b7e4bc857418112
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-02-01 19:15:36 +0600

    fibonacchi with loop

---
+++ fibinacchieithloop.js
@@ -0,0 +1,10 @@
+function fibonacchi(n){
+
+    let fib = [0,1]
+    for(let i = 2 ; i<n;i++){
+        fib[i] = fib[i-2] + fib[i-1]
+    }
+    return fib
+}
+
+console.log(fibonacchi(10))
--- fibopnacchiwithrecursion.js
+++ fibopnacchiwithrecursion.js
@@ -7,11 +7,7 @@ function fibonacchi(n){
         return fibonacchi(n-1)+fibonacchi(n-2)
 
     }
-    // let fib = [0,1]
-    // for(let i = 2 ; i<n;i++){
-    //     fib[i] = fib[i-2] + fib[i-1]
-    // }
-    // return fib
+
 }
 
 console.log(fibonacchi(10))
